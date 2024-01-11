4ee56e0118ec832251d26f83cc16f985fb551945
Poman <Poman@DESKTOP-8IJUT9Q>
2024-01-11 15:53:07 +0800

    Add project files.

---
+++ WaSuperFilterApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using WaSuperFilterApp;
+
+namespace WaSuperFilterAppApp
+{
+    internal static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+             
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+
+         
+
+         
+         
+    }
+}
---
+++ WaSuperFilterApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+ 
+[assembly: AssemblyTitle("Whatsapp Super Filter Pro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Whatsapp Super Filter Pro")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("https://codecanyon.net/user/ionicstudio/portfolio")]
+[assembly: AssemblyCulture("")]
+
+ 
+[assembly: ComVisible(false)]
+
+ 
+[assembly: Guid("3c8a82aa-3870-41d0-95be-1923ba9edf97")]
+
+ 
+[assembly: AssemblyVersion("8.3.8.0")]
+[assembly: AssemblyFileVersion("8.3.8.0")]
+
